20d3c106f8671aa0c6f585ff700cdbe43e7d55ff
Jerlon Abayon <2024001@MLHUILLIER.LOCAL>
2024-06-08 09:32:50 +0800

    fixed

--- front-end/src/app/(website)/page.tsx
+++ front-end/src/app/(website)/page.tsx
@@ -5,31 +5,31 @@ import Faqs from '@/components/landing/faqs';
 import Footer from '@/components/landing/footer';
 
 const LandingPage = () => {
-  const [message, setMessage] = useState("Loading...");
+  // const [message, setMessage] = useState("Loading...");
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data); // This will log the response data to the console
-        setMessage(data.message);
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
-        setMessage("Failed to load message");
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/api/home")
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       console.log(data); // This will log the response data to the console
+  //       setMessage(data.message);
+  //     })
+  //     .catch(error => {
+  //       console.error('Fetch error:', error);
+  //       setMessage("Failed to load message");
+  //     });
+  // }, []);
 
   return (
     <div>
       <Header />
       <div>Landing Page</div>
-      <div>{message}</div>
+      {/* <div>{message}</div> */}
       <Faqs />
       <Footer />
     </div>
